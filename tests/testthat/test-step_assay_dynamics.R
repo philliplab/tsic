28e47ea985535bc67bc1fb7cfd1747804ac819cb
Phillip Labuschagne <philliplab@gmail.com>
2019-08-05 09:58:17 +0200

    ADD tests that step dynamics step in the right place

--- tests/testthat/test-step_assay_dynamics.R
+++ tests/testthat/test-step_assay_dynamics.R
@@ -5,4 +5,13 @@ test_that("Test that the diagnostic_delany works: diagnostic delay = 20", {
   expect_equal(x5, 0)
   x50 <- step_assay_dynamics(50, 20)
   expect_equal(x50, 1)
+
+  x20 <- step_assay_dynamics(20, 20)
+  expect_equal(x20, 0)
+
+  x19.999 <- step_assay_dynamics(19.999, 20)
+  expect_equal(x19.999, 0)
+
+  x20.001 <- step_assay_dynamics(20.001, 20)
+  expect_equal(x20.001, 1)
 })
