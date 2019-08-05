4b797ffd8ae584a904a9fbe547db03109e7daf28
Phillip Labuschagne <philliplab@gmail.com>
2019-08-05 07:51:25 +0200

    ADD tests for abs_spread parameter of linear_assay_dynamics

--- tests/testthat/test-linear_assay_dynamics.R
+++ tests/testthat/test-linear_assay_dynamics.R
@@ -8,17 +8,32 @@ test_that("Test that the spread parameter works: diagnostic delay = 20, spread =
   x20 <- linear_assay_dynamics(20, 20, 0.1)
   expect_equal(x20, 0.5)
 
-  x <- linear_assay_dynamics(19, 20, 0.1)
-  expect_equal(x, 0)
-  x <- linear_assay_dynamics(21, 20, 0.1)
-  expect_equal(x, 1)
+  x19 <- linear_assay_dynamics(19, 20, 0.1)
+  expect_equal(x19, 0)
+  x21 <- linear_assay_dynamics(21, 20, 0.1)
+  expect_equal(x21, 1)
 
-  x <- linear_assay_dynamics(19.5, 20, 0.1)
-  expect_equal(x, 0.25)
-  x <- linear_assay_dynamics(20.5, 20, 0.1)
-  expect_equal(x, 0.75)
+  x19.5 <- linear_assay_dynamics(19.5, 20, 0.1)
+  expect_equal(x19.5, 0.25)
+  x20.5 <- linear_assay_dynamics(20.5, 20, 0.1)
+  expect_equal(x20.5, 0.75)
 })
 
-test_that("TODO Test that the abs_spread parameter works: diagnostic delay = TODO, spread = TODO", {
-  expect_equal(0, 1)
+test_that("Test that the abs_spread parameter works: diagnostic delay = 10, abs_spread = 5", {
+  x5 <- linear_assay_dynamics(5, 10, abs_spread = 5)
+  expect_equal(x5, 0)
+  x50 <- linear_assay_dynamics(50, 10, abs_spread = 5)
+  expect_equal(x50, 1)
+  x10 <- linear_assay_dynamics(10, 10, abs_spread = 5)
+  expect_equal(x10, 0.5)
+
+  x7.5 <- linear_assay_dynamics(7.5, 10, abs_spread = 5)
+  expect_equal(x7.5, 0)
+  x12.5 <- linear_assay_dynamics(12.5, 10, abs_spread = 5)
+  expect_equal(x12.5, 1)
+
+  x8.75 <- linear_assay_dynamics(8.75, 10, abs_spread = 5)
+  expect_equal(x8.75, 0.25)
+  x11.25 <- linear_assay_dynamics(11.25, 10, abs_spread = 5)
+  expect_equal(x11.25, 0.75)
 })
