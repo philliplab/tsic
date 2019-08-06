c2ae4627524490d5474ab21b17d1763b01c8dc59
Phillip Labuschagne <philliplab@gmail.com>
2019-08-06 13:05:08 +0200

    ADD tests to compensate for my lack of experience with closures

--- tests/testthat/test-result_interpretation.R
+++ tests/testthat/test-result_interpretation.R
@@ -16,3 +16,40 @@ test_that("assay_result_interpreter gets build correctly", {
   expect_equal(foo(20), 0)
   expect_equal(foo(2), 1)
 })
+
+
+test_that("mucking about in the outer scope does not mess with the closure", {
+  # These tests are only necessary because I am not that familiar with closures
+  assay_dynamics <- get_assay_dynamics(assay = 'step_unit_testing')
+  result <- '+'
+  foo <- construct_assay_result_interpreter(assay_dynamics = assay_dynamics,
+                                            result = result)
+  expect_type(foo, 'closure')
+  expect_equal(foo(20), 1)
+  expect_equal(foo(2), 0)
+  
+  assay_dynamics$parameters$diagnostic_delay <- 100
+  expect_equal(foo(20), 1)
+  assay_dynamics$parameters$diagnostic_delay <- 1.5
+  expect_equal(foo(2), 0)
+  
+  result <- '-'
+  expect_equal(foo(20), 1)
+  expect_equal(foo(2), 0)
+  
+  result <- '-'
+  foo <- construct_assay_result_interpreter(assay_dynamics = assay_dynamics,
+                                            result = result)
+  expect_type(foo, 'closure')
+  expect_equal(foo(20), 0)
+  expect_equal(foo(2), 1)
+  
+  assay_dynamics$parameters$diagnostic_delay <- 100
+  expect_equal(foo(20), 0)
+  assay_dynamics$parameters$diagnostic_delay <- 1.5
+  expect_equal(foo(2), 1)
+  
+  result <- '+'
+  expect_equal(foo(20), 0)
+  expect_equal(foo(2), 1)
+})
