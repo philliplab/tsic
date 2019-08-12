63db89f7aa17177a54cfe4ebae9621b2d54e3452
Phillip Labuschagne <philliplab@gmail.com>
2019-08-12 13:07:32 +0200

    CONVERT get_scatterpoints tests to use dates

--- tests/testthat/test-result_interpretation.R
+++ tests/testthat/test-result_interpretation.R
@@ -23,142 +23,158 @@ test_that("assay_result_interpreter gets build correctly", {
   expect_equal(foo('2018-06-30'), 1)
 })
 
-#
-#test_that("mucking about in the outer scope does not mess with the closure", {
-#  # These tests are only necessary because I am not that familiar with closures
-#  assay_dynamics <- get_assay_dynamics(assay = 'step_unit_testing')
-#  result <- '+'
-#  foo <- construct_assay_result_interpreter(assay_dynamics = assay_dynamics,
-#                                            result = result)
-#  expect_type(foo, 'closure')
-#  expect_equal(foo(20), 1)
-#  expect_equal(foo(2), 0)
-#  
-#  assay_dynamics$parameters$diagnostic_delay <- 100
-#  expect_equal(foo(20), 1)
-#  assay_dynamics$parameters$diagnostic_delay <- 1.5
-#  expect_equal(foo(2), 0)
-#  
-#  result <- '-'
-#  expect_equal(foo(20), 1)
-#  expect_equal(foo(2), 0)
-#  
-#  result <- '-'
-#  foo <- construct_assay_result_interpreter(assay_dynamics = assay_dynamics,
-#                                            result = result)
-#  expect_type(foo, 'closure')
-#  expect_equal(foo(20), 0)
-#  expect_equal(foo(2), 1)
-#  
-#  assay_dynamics$parameters$diagnostic_delay <- 100
-#  expect_equal(foo(20), 0)
-#  assay_dynamics$parameters$diagnostic_delay <- 1.5
-#  expect_equal(foo(2), 1)
-#  
-#  result <- '+'
-#  expect_equal(foo(20), 0)
-#  expect_equal(foo(2), 1)
-#})
-#
-#test_that("get_scatterpoints work in normal circumstances", {
-#  x <- 0:10/1000
-#  fun <- function(x){return(x)}
-#  scatterpoints <- get_scatterpoints(fun = fun, seedpoints = x)
-#  expect_true(all(scatterpoints$x == x))
-#  expect_true(all(scatterpoints$y == x))
-#})
-#
-#test_that("get_scatterpoints adds in enough intervals for a sloped line between two points", {
-#  x <- 0:1/40
-#  fun <- function(x){return(x)}
-#  scatterpoints <- get_scatterpoints(fun = fun, seedpoints = x, 
-#                                     max_delta = 0.02, min_length = 0.01,
-#                                     n_new_segments = 20, verbose = FALSE)
-#  delta <- abs(scatterpoints$y[1:(length(scatterpoints$y)-1)] - scatterpoints$y[2:length(scatterpoints$y)])
-#  int_lengths <- abs(scatterpoints$x[1:(length(scatterpoints$x)-1)] - scatterpoints$x[2:length(scatterpoints$x)])
-#  expect_true(all(sort(names(scatterpoints)) == c('x', 'y')))
-#  expect_equal(length(scatterpoints$x), length(unique(scatterpoints$x)))
-#  expect_equal(length(scatterpoints$x), length(scatterpoints$y))
-#  expect_true(all(delta < 0.02)) 
-#  expect_true(all(int_lengths > 0.01/20)) 
-#  expect_equal(length(scatterpoints$x), 20)
-#})
-#
-#test_that("get_scatterpoints adds in enough intervals for a sloped line between many points", {
-#  x <- 0:10/40
-#  fun <- function(x){return(x)}
-#  scatterpoints <- get_scatterpoints(fun = fun, seedpoints = x, 
-#                                     max_delta = 0.02, min_length = 0.01,
-#                                     n_new_segments = 20, verbose = FALSE)
-#  delta <- abs(scatterpoints$y[1:(length(scatterpoints$y)-1)] - scatterpoints$y[2:length(scatterpoints$y)])
-#  int_lengths <- abs(scatterpoints$x[1:(length(scatterpoints$x)-1)] - scatterpoints$x[2:length(scatterpoints$x)])
-#  expect_true(all(sort(names(scatterpoints)) == c('x', 'y')))
-#  expect_equal(length(scatterpoints$x), length(unique(scatterpoints$x)))
-#  expect_equal(length(scatterpoints$x), length(scatterpoints$y))
-#  expect_true(all(delta < 0.02)) 
-#  expect_true(all(int_lengths > 0.01/20)) 
-#  expect_equal(length(scatterpoints$x), 191)
-#})
-#
-#test_that("min_length prevents get_scatterpoints from adding in too many points", {
-#  x <- 0:1/40
-#  fun <- function(x){return(x)}
-#  scatterpoints <- get_scatterpoints(fun = fun, seedpoints = x, 
-#                                     max_delta = 0.0001, min_length = 0.01,
-#                                     n_new_segments = 20, verbose = FALSE)
-#  delta <- abs(scatterpoints$y[1:(length(scatterpoints$y)-1)] - scatterpoints$y[2:length(scatterpoints$y)])
-#  int_lengths <- abs(scatterpoints$x[1:(length(scatterpoints$x)-1)] - scatterpoints$x[2:length(scatterpoints$x)])
-#  expect_true(all(sort(names(scatterpoints)) == c('x', 'y')))
-#  expect_equal(length(scatterpoints$x), length(unique(scatterpoints$x)))
-#  expect_equal(length(scatterpoints$x), length(scatterpoints$y))
-#  expect_true(all(int_lengths > 0.01/20)) 
-#  expect_equal(length(scatterpoints$x), 20)
-#})
-#
-#test_that("get_scatterpoints can recurse more than once", {
-#  x <- 0:1/40
-#  fun <- function(x){return(x)}
-#  scatterpoints <- get_scatterpoints(fun = fun, seedpoints = x, 
-#                                     max_delta = 0.0001, min_length = 0.001,
-#                                     n_new_segments = 20, verbose = FALSE)
-#  delta <- abs(scatterpoints$y[1:(length(scatterpoints$y)-1)] - scatterpoints$y[2:length(scatterpoints$y)])
-#  int_lengths <- abs(scatterpoints$x[1:(length(scatterpoints$x)-1)] - scatterpoints$x[2:length(scatterpoints$x)])
-#  expect_true(all(sort(names(scatterpoints)) == c('x', 'y')))
-#  expect_equal(length(scatterpoints$x), length(unique(scatterpoints$x)))
-#  expect_equal(length(scatterpoints$x), length(scatterpoints$y))
-#  expect_true(all(delta < 0.0001)) 
-#  expect_true(all(int_lengths > 0.001/20)) 
-#  expect_equal(length(scatterpoints$x), 362)
-#})
-#
-#test_that("get_scatterpoints zoom in on the step", {
-#  assay_dynamics <- get_assay_dynamics(assay = 'step_unit_testing')
-#  result <- '+'
-#  foo <- construct_assay_result_interpreter(assay_dynamics = assay_dynamics,
-#                                            result = result)
-#  x <- 1:20
-#  scatterpoints <- get_scatterpoints(fun = foo, seedpoints = x, 
-#                                     max_delta = 0.01, min_length = 0.01,
-#                                     n_new_segments = 20, verbose = FALSE)
-#
-#  expect_equal(sum(scatterpoints$x <= 9), length(1:9))
-#  expect_equal(sum(scatterpoints$x >= 11), length(11:20))
-#  expect_true(sum(scatterpoints$x>=9 & scatterpoints$x <= 11) > 19)
-#  
-#  assay_dynamics <- get_assay_dynamics(assay = 'step_unit_testing')
-#  result <- '-'
-#  foo <- construct_assay_result_interpreter(assay_dynamics = assay_dynamics,
-#                                            result = result)
-#  x <- 1:20
-#  scatterpoints <- get_scatterpoints(fun = foo, seedpoints = x, 
-#                                     max_delta = 0.01, min_length = 0.01,
-#                                     n_new_segments = 20, verbose = FALSE)
-#
-#  expect_equal(sum(scatterpoints$x <= 9), length(1:9))
-#  expect_equal(sum(scatterpoints$x >= 11), length(11:20))
-#  expect_true(sum(scatterpoints$x>=9 & scatterpoints$x <= 11) > 19)
-#})
-#
+
+test_that("mucking about in the outer scope does not mess with the closure", {
+  # These tests are only necessary because I am not that familiar with closures
+  assay_dynamics <- get_assay_dynamics(assay = 'step_unit_testing')
+  result <- '+'
+  foo <- construct_assay_result_interpreter(assay_dynamics = assay_dynamics,
+                                            result = result,
+                                            sample_date = '2018-06-01')
+  expect_type(foo, 'closure')
+  expect_equal(foo('2018-05-01'), 1)
+  expect_equal(foo('2018-05-30'), 0)
+  
+  assay_dynamics$parameters$diagnostic_delay <- 100
+  expect_equal(foo('2018-05-01'), 1)
+  assay_dynamics$parameters$diagnostic_delay <- 1.5
+  expect_equal(foo('2018-05-30'), 0)
+  
+  result <- '-'
+  expect_equal(foo('2018-05-01'), 1)
+  expect_equal(foo('2018-05-30'), 0)
+  
+  result <- '-'
+  foo <- construct_assay_result_interpreter(assay_dynamics = assay_dynamics,
+                                            result = result,
+                                            sample_date = '2018-06-01')
+  expect_type(foo, 'closure')
+  expect_equal(foo('2018-05-01'), 0)
+  expect_equal(foo('2018-05-30'), 1)
+  
+  assay_dynamics$parameters$diagnostic_delay <- 100
+  expect_equal(foo('2018-05-01'), 0)
+  assay_dynamics$parameters$diagnostic_delay <- 1.5
+  expect_equal(foo('2018-05-30'), 1)
+  
+  result <- '+'
+  expect_equal(foo('2018-05-01'), 0)
+  expect_equal(foo('2018-05-30'), 1)
+})
+
+test_that("get_scatterpoints work in normal circumstances", {
+  x <- 0:10/1000
+  fun <- function(x){return(x)}
+  scatterpoints <- get_scatterpoints(fun = fun, seedpoints = x)
+  expect_true(all(scatterpoints$x == x))
+  expect_true(all(scatterpoints$y == x))
+})
+
+test_that("get_scatterpoints adds in enough intervals for a sloped line between two points", {
+  x <- 0:1/40
+  fun <- function(x){return(x)}
+  scatterpoints <- get_scatterpoints(fun = fun, seedpoints = x, 
+                                     max_delta = 0.02, min_length = 0.01,
+                                     n_new_segments = 20, verbose = FALSE)
+  delta <- abs(scatterpoints$y[1:(length(scatterpoints$y)-1)] - scatterpoints$y[2:length(scatterpoints$y)])
+  int_lengths <- abs(scatterpoints$x[1:(length(scatterpoints$x)-1)] - scatterpoints$x[2:length(scatterpoints$x)])
+  expect_true(all(sort(names(scatterpoints)) == c('x', 'y')))
+  expect_equal(length(scatterpoints$x), length(unique(scatterpoints$x)))
+  expect_equal(length(scatterpoints$x), length(scatterpoints$y))
+  expect_true(all(delta < 0.02)) 
+  expect_true(all(int_lengths > 0.01/20)) 
+  expect_equal(length(scatterpoints$x), 20)
+})
+
+test_that("get_scatterpoints adds in enough intervals for a sloped line between many points", {
+  x <- 0:10/40
+  fun <- function(x){return(x)}
+  scatterpoints <- get_scatterpoints(fun = fun, seedpoints = x, 
+                                     max_delta = 0.02, min_length = 0.01,
+                                     n_new_segments = 20, verbose = FALSE)
+  delta <- abs(scatterpoints$y[1:(length(scatterpoints$y)-1)] - scatterpoints$y[2:length(scatterpoints$y)])
+  int_lengths <- abs(scatterpoints$x[1:(length(scatterpoints$x)-1)] - scatterpoints$x[2:length(scatterpoints$x)])
+  expect_true(all(sort(names(scatterpoints)) == c('x', 'y')))
+  expect_equal(length(scatterpoints$x), length(unique(scatterpoints$x)))
+  expect_equal(length(scatterpoints$x), length(scatterpoints$y))
+  expect_true(all(delta < 0.02)) 
+  expect_true(all(int_lengths > 0.01/20)) 
+  expect_equal(length(scatterpoints$x), 191)
+})
+
+test_that("min_length prevents get_scatterpoints from adding in too many points", {
+  x <- 0:1/40
+  fun <- function(x){return(x)}
+  scatterpoints <- get_scatterpoints(fun = fun, seedpoints = x, 
+                                     max_delta = 0.0001, min_length = 0.01,
+                                     n_new_segments = 20, verbose = FALSE)
+  delta <- abs(scatterpoints$y[1:(length(scatterpoints$y)-1)] - scatterpoints$y[2:length(scatterpoints$y)])
+  int_lengths <- abs(scatterpoints$x[1:(length(scatterpoints$x)-1)] - scatterpoints$x[2:length(scatterpoints$x)])
+  expect_true(all(sort(names(scatterpoints)) == c('x', 'y')))
+  expect_equal(length(scatterpoints$x), length(unique(scatterpoints$x)))
+  expect_equal(length(scatterpoints$x), length(scatterpoints$y))
+  expect_true(all(int_lengths > 0.01/20)) 
+  expect_equal(length(scatterpoints$x), 20)
+})
+
+test_that("get_scatterpoints can recurse more than once", {
+  x <- 0:1/40
+  fun <- function(x){return(x)}
+  scatterpoints <- get_scatterpoints(fun = fun, seedpoints = x, 
+                                     max_delta = 0.0001, min_length = 0.001,
+                                     n_new_segments = 20, verbose = FALSE)
+  delta <- abs(scatterpoints$y[1:(length(scatterpoints$y)-1)] - scatterpoints$y[2:length(scatterpoints$y)])
+  int_lengths <- abs(scatterpoints$x[1:(length(scatterpoints$x)-1)] - scatterpoints$x[2:length(scatterpoints$x)])
+  expect_true(all(sort(names(scatterpoints)) == c('x', 'y')))
+  expect_equal(length(scatterpoints$x), length(unique(scatterpoints$x)))
+  expect_equal(length(scatterpoints$x), length(scatterpoints$y))
+  expect_true(all(delta < 0.0001)) 
+  expect_true(all(int_lengths > 0.001/20)) 
+  expect_equal(length(scatterpoints$x), 362)
+})
+
+test_that("get_scatterpoints zoom in on the step", {
+  assay_dynamics <- get_assay_dynamics(assay = 'step_unit_testing')
+  result <- '+'
+  sample_date <- lubridate::ymd('2015-01-01')
+  range_start <- lubridate::ymd('2014-12-01')
+  foo <- construct_assay_result_interpreter(assay_dynamics = assay_dynamics,
+                                            result = result,
+                                            sample_date = sample_date)
+  x <- range_start:sample_date
+  scatterpoints <- get_scatterpoints(fun = foo, seedpoints = x, 
+                                     max_delta = 0.01, min_length = 0.01,
+                                     n_new_segments = 20, verbose = FALSE)
+
+  date_less_diagnostic_delay <- sample_date - lubridate::days(assay_dynamics$params$diagnostic_delay)
+  day_before <- (date_less_diagnostic_delay-lubridate::days(1))
+  day_after <- (date_less_diagnostic_delay+lubridate::days(1))
+
+  expect_equal( sum(scatterpoints$x <= day_before), 
+                length(range_start:day_before) )
+  expect_equal( sum(scatterpoints$x >= day_after), 
+                length(day_after:sample_date) )
+  expect_true( sum( scatterpoints$x >= day_before & 
+                    scatterpoints$x <= day_after) > 19 )
+  
+  assay_dynamics <- get_assay_dynamics(assay = 'step_unit_testing')
+  result <- '-'
+  foo <- construct_assay_result_interpreter(assay_dynamics = assay_dynamics,
+                                            result = result,
+                                            sample_date = sample_date)
+  x <- range_start:sample_date
+  scatterpoints <- get_scatterpoints(fun = foo, seedpoints = x, 
+                                     max_delta = 0.01, min_length = 0.01,
+                                     n_new_segments = 20, verbose = FALSE)
+
+  expect_equal( sum(scatterpoints$x <= day_before), 
+                length(range_start:day_before) )
+  expect_equal( sum(scatterpoints$x >= day_after), 
+                length(day_after:sample_date) )
+  expect_true( sum( scatterpoints$x >= day_before & 
+                    scatterpoints$x <= day_after) > 19 )
+})
+
 #test_that('reduce_x_points works in a trivial case', {
 #  x <- 1:10
 #  y <- rep(0, 10)
