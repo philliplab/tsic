f53bbcd9fcdcbc778c5cb855c9e55ac12a4718c5
Phillip Labuschagne <philliplab@gmail.com>
2019-10-14 13:22:28 -0400

    ADD sim_visit_dates function

--- R/sims.R
+++ R/sims.R
@@ -10,6 +10,7 @@
 #' @param n The number of people to simulate
 #' @param rates The parameters for the exponential distributions of each of the six stages. ( default = 'fiebig' which gets replaced with list(l1 = 5.0, l2 = 5.3, l3 = 3.2, l4 = 5.6, l5 = 69.5) )
 #' @param eclipse_distribution A list with elements distr pointing to the function allowing draws from the eclipse distribution ( default = function(n, shape, scale, location) {location + rweibull(n, shape = shape, scale = scale)} ) and the parameters of said distribution ( default = list(shape = 1.35, scale = 9, location = 4.8) )
+#' @export
 
 sim_durations <- function(n = 30, rates = 'fiebig',
                             eclipse_distribution = list(
@@ -42,3 +43,48 @@ sim_durations <- function(n = 30, rates = 'fiebig',
   }
   return(durations)
 }
+
+#' Simulates the visit dates around a set of transitions
+#'
+#' @param durations The data.frame of transitions as produced by sim_durations.
+#' @param inf_date The infection date. In this version, all infections occur on the same date. This makes comparisions easy also.
+#' @param n_before_infection The number of visits before infection.
+#' @param gap_distribution A function that generates the n gaps between the next n visits.
+#' @export
+
+sim_visit_dates <- function(durations, 
+                            inf_date = as.numeric(as.Date('2015-05-01')),
+                            n_before_infection = 2,
+                            gap_distribution = function(n){rnorm(n, 30, 3)}){
+  if (FALSE){
+    durations <- sim_durations()
+    inf_date <- as.numeric(as.Date('2015-05-01'))
+    n_before_infection <- 2
+    gap_distribution <- function(n, mu = 30, sig = 3){rnorm(n, mu, sig)}
+    c_ptid <- 1
+  }
+  all_visit_dates <- NULL
+
+  for (c_ptid in sort(unique(durations$ptid))){
+    pot_gaps <- c(0, cumsum(gap_distribution(max(n_before_infection + 5, 10))))
+    inf_offset <- runif(1, 1, pot_gaps[n_before_infection] - 1)
+    v1_date <- inf_date - (inf_offset + pot_gaps[n_before_infection])
+    visit_dates <- v1_date + pot_gaps
+    last_transition <- inf_date + sum(durations[ptid == c_ptid, -1])
+    while (last_transition > max(visit_dates)){
+      pot_gaps <- cumsum(gap_distribution(max(n_before_infection + 5, 10)))
+      visit_dates <- c(visit_dates, max(visit_dates) + pot_gaps)
+    }
+    all_visit_dates <- rbind(all_visit_dates,
+      data.frame(ptid = c_ptid,
+                 visit_id = 1:length(visit_dates),
+                 visit_date = sort(visit_dates)))
+  }
+  return(all_visit_dates)
+}
+
+
+
+
+
+
