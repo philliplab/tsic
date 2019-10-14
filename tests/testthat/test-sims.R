d5a97596d4eccce650ed065a792c746662597460
Phillip Labuschagne <philliplab@gmail.com>
2019-10-14 08:37:03 -0400

    ADD basic tests for sim_durations

---
+++ tests/testthat/test-sims.R
@@ -0,0 +1,24 @@
+context("test-sims")
+
+test_that("Test sim_durations is sane", {
+  if (FALSE){
+    devtools::load_all()
+  }
+  durations <- sim_durations(n = 30,
+                             rates = 'fiebig',
+                             eclipse_distribution <- list(
+                               distr = function(n, shape, scale, location) {
+                                 location + rweibull(n, shape = shape, scale = scale)
+                               },
+                               params = list(shape = 1.35, scale = 9, location = 4.8)))
+  expect_equal(class(durations), 'data.frame')
+  expect_equal(names(durations), c('ptid', 'eclipse', 'l1', 'l2', 'l3', 'l4', 'l5'))
+  stage_classes <- apply(durations[,-1], 2, class)
+  expect_equal(unique(stage_classes), 'numeric')
+
+  expect_equal(nrow(durations), 30)
+  for (c_col in c('eclipse', 'l1', 'l2', 'l3', 'l4', 'l5')){
+    expect_true(all(durations[, c_col] > 0))
+  }
+})
+
