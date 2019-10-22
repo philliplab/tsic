6a7a5afc9872cf5d1e950f6ad8ff930aebc24cdc
Phillip Labuschagne <philliplab@gmail.com>
2019-10-22 09:39:00 -0400

    ADD detailed tests for compute_extra_tiles in specific cases

--- tests/testthat/test-lb_med_ub_calcs.R
+++ tests/testthat/test-lb_med_ub_calcs.R
@@ -69,6 +69,44 @@ test_that('estimate_lb_med_ub works on basic functions', {
   expect_lte((res$ub  - qexp(0.975))^2, 0.0001)
 })
 
+test_that('extra_tiles of estimate_lb_med_ub works on basic functions', {
+  if (FALSE){
+  devtools::load_all()
+  }
+  fun <- function(x){ifelse(x<0, 0, ifelse(x>100, 0, 1/100))}
+  range_to_int <- trim_range(fun = fun, range_start = -1000, range_end = 1000)
+  res <- estimate_lb_med_ub(fun = fun,
+                            range_start = range_to_int$range_start,
+                            range_end = range_to_int$range_end,
+                            extra_tiles = (1:9)/10)
+  expect_lte((res$lb  - 2.5 )^2, 0.0001)
+  expect_lte((res$med - 50  )^2, 0.0001)
+  expect_lte((res$ub  - 97.5)^2, 0.0001)
+  for (i in 1:9){
+    expect_lte((res$extra_computed_tiles[[as.character(i/10)]]$minimum  - i*10)^2, 0.0001)
+  }
+
+  range_to_int <- trim_range(fun = dnorm, range_start = -10000, range_end = 10000)
+  res <- estimate_lb_med_ub(fun = dnorm,
+                            range_start = range_to_int$range_start,
+                            range_end = range_to_int$range_end,
+                            extra_tiles = (1:9)/10)
+  expect_lte((res$lb  - qnorm(0.025))^2, 0.0001)
+  expect_lte((res$med - qnorm(0.5)  )^2, 0.0001)
+  expect_lte((res$ub  - qnorm(0.975))^2, 0.0001)
+  for (i in 1:9){
+    expect_lte((res$extra_computed_tiles[[as.character(i/10)]]$minimum  - qnorm(i/10))^2, 0.0001)
+  }
+
+  range_to_int <- trim_range(fun = dexp, range_start = -10000, range_end = 10000)
+  res <- estimate_lb_med_ub(fun = dexp,
+                            range_start = range_to_int$range_start,
+                            range_end = range_to_int$range_end)
+  expect_lte((res$lb  - qexp(0.025))^2, 0.0001)
+  expect_lte((res$med - qexp(0.5)  )^2, 0.0001)
+  expect_lte((res$ub  - qexp(0.975))^2, 0.0001)
+})
+
 test_that('estimate_lb_med_ub works with diagnostic histories', {
   # weibull dynamics aptima + architect
   ihist <- structure(list(ptid = c("p01", "p01", "p01", "p01", "p01", "p01",
