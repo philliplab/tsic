029ccf0c42831c72753a9401b927e36ff314d474
Phillip Labuschagne <philliplab@gmail.com>
2020-01-07 14:47:13 -0500

    ADDs test for daily grid for weibull cdf

--- tests/testthat/test-daily-grid.R
+++ tests/testthat/test-daily-grid.R
@@ -10,4 +10,15 @@ test_that('Daily Grid works on known distributions', {
   expect_equal(sum(x$daily_probs), 1)
   expect_equal(17400, min(x$starts_of_daily_intervals))
   expect_equal(17599, max(x$starts_of_daily_intervals))
+
+  agg_fun <- function(x){dweibull(x, 10, 10)}
+  x <- compute_daily_grid(agg_fun, 1, 0, 20)
+  expect_equal(sum(x$daily_probs), 1)
+  expect_equal(0, min(x$starts_of_daily_intervals))
+  expect_equal(19, max(x$starts_of_daily_intervals))
 })
+
+
+
+
+
