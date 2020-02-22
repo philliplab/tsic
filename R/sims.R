15c4a6c801aae7550ef0f7251bfe57035aa80e6f
Phillip Labuschagne <philliplab@gmail.com>
2020-02-22 14:55:23 -0500

    ADD sim_dx_results function to tsic

--- R/sims.R
+++ R/sims.R
@@ -1,3 +1,52 @@
+#' Simulates a visit's diagnostic results
+#'
+#' Given the time since infection, this function will draw from the window period distributions of the specified assays to obtain a set of diagnostic test result.
+#'
+#' The first negative test result will stop the procedure since then it is known that all other assays will also produce negative results.
+#'
+#' @param tsi The time since infection in days. tsi = 0 is the day of infection.
+#' @param list_of_assays ORDERED list of assays for which test results should be produced. They must be ordered with the fastest assay first (fastest = assay with the shortest window period)
+#' @export
+
+sim_dx_results <- function(tsi, list_of_assays){
+
+  dx_results <- list()
+  
+  # check that assay list is in correct order
+  for (indx in 1:(length(list_of_assays)-1)){
+    assay1 <- get_assay_dynamics(list_of_assays[indx])
+    assay2 <- get_assay_dynamics(list_of_assays[indx+1])
+    res <- which_is_faster(assay1, assay2)
+    if (assay1$short_assayname != res$faster$short_assayname){
+      stop('assay list must be from fastest to slowest')
+    }
+  }
+  
+  # draw result for each assay until first negative result
+  for (c_assay in list_of_assays){
+    assay_dynamics <- get_assay_dynamics(c_assay)
+  
+    evaluate_dynamics <- function(x){ #x is time since infection event
+      assay_dynamics$params$x <- x
+      return(do.call(assay_dynamics$fun, assay_dynamics$params))
+    }
+  
+    pos_prob <- evaluate_dynamics(tsi)
+    if (runif(1) < pos_prob){
+      dx_results[[c_assay]] <- '+'
+    } else {
+      dx_results[[c_assay]] <- '-'
+      return(dx_results)
+    }
+  }
+  return(dx_results)
+}
+
+
+
+
+
+
 #' Simulates the durations times between fiebig stages
 #'
 #' For each patients, draw an eclispe duration from the supplied eclipse_distribution,
