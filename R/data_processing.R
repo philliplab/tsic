b95bcadc20eabeb1adb8473d03a899862ee29bd4
Phillip Labuschagne <philliplab@gmail.com>
2020-04-30 10:39:27 -0400

    ADD basic version of compute_result_summary

--- R/data_processing.R
+++ R/data_processing.R
@@ -124,3 +124,66 @@ visit_labeller <- function(ihist){
   }
   return(ihist)
 }
+
+#' Computes a summary of the results of a single assay for an ihist
+#'
+#' Given an ihist and an assay of interest, compute the observed LN and FP dates for that assay. Optionally, slower (faster) assays are inspected to see if earlier (later) LN (FP) dates can be deduced (only useful in the case where some tests are not performed at some visits).
+#'
+#' @return A single row of a data.frame with the columns:
+#' \itemize{
+#'   \item ptid - The participant identifier read from the ihist
+#'   \item assay_name - The name of the assay this row describes. From the assay_name input to the function.
+#'   \item obs_rel_LN - Observed LN date for the assay relative to the overall first positive for the participant. 
+#'   \item ded_rel_LN - Deduced LN date (from faster assays) for this assay relative to the overall first positive for the participant. 
+#'   \item LN_ded_from - The assay name from which the LN was deduced. Will always be the fastest assay that produced the earliest LN date.
+#'   \item obs_rel_FP - Observed FP date for the assay relative to the overall first positive for the participant. 
+#'   \item ded_rel_FP - Deduced FP date (from slower assays) for this assay relative to the overall first positive for the participant. 
+#'   \item FP_ded_from - The assay name from which the FP was deduced. Will always be the fastest assay that produced the earliest FP date.
+#'   \item rel_LN - The maximum of the observed and deduced LN dates. This will ensure that the interval during which the assay transitioned from negative to positive is as short as possible by factoring in the structural knowledge we assume about the assays.
+#'   \item rel_FP - The minimun of the observed and deduced FP dates. This will ensure that the interval during which the assay transitioned from negative to positive is as short as possible by factoring in the structural knowledge we assume about the assays.
+#'   \item LN_FP_gap - The width of the interval between which the assay transitioned from negative to positive.
+#' }
+#'
+#' @param ihist The individual diagnostic history from which an assay's results must be summarized.
+#' @param assay_name The name of the assay to summarize.
+#' @param faster_assays The assays with shorter window periods than this assay that should be checked if a later LN result can be deduced.
+#' @return
+
+compute_result_summary <- function(ihist, assay_name, faster_assays = NULL){
+  warning('in progress - somethings still need to be figured out')
+  fp_date <- min(subset(ihist, test == assay_name & result == '+')$sample_date)
+  ln_date <- max(subset(ihist, test == assay_name & sample_date < fp_date)$sample_date)
+  rel_fp_date <- min(subset(ihist, test == assay_name & result == '+')$rel_sample_date)
+  rel_ln_date <- max(subset(ihist, test == assay_name & rel_sample_date < rel_fp_date)$rel_sample_date)
+  
+  # compute the lastest faster LN date
+  rel_latest_faster_ln_date <- -Inf
+  for (c_assay in faster_assays){
+    rel_c_ln_date <- max(subset(ihist, test == c_assay & result == '-')$rel_sample_date)
+    if (rel_latest_faster_ln_date < rel_c_ln_date){
+      rel_latest_faster_ln_date <- rel_c_ln_date
+    }
+  }
+
+  # check that fp date is reasonable given other fp dates
+  faster_assays <- c(faster_assays, assay_name)
+  assay_sane <- TRUE
+  for (c_assay in faster_assays){
+    c_fp_date <- min(subset(ihist, test == c_assay & result == '+')$sample_date)
+    assay_sane <- assay_sane & (max(subset(ihist, test == c_assay & result == '-')$sample_date) < c_fp_date)
+  }
+
+  assay_not_inf <- fp_date != Inf & ln_date != -Inf
+  return(data.frame(ptid = unique(ihist$ptid),
+                    test = assay_name,
+                    assay_sane = assay_sane,
+                    ln_date = ln_date,
+                    fp_date = fp_date,
+                    ln_fp_gap = rel_fp_date - max(rel_ln_date, rel_latest_faster_ln_date),
+                    rel_ln_date = rel_ln_date,
+                    rel_fp_date = rel_fp_date,
+                    rel_latest_faster_ln_date = rel_latest_faster_ln_date,
+                    useful_rel_ln_date = max(rel_ln_date, rel_latest_faster_ln_date),
+                    assay_not_inf = assay_not_inf,
+                    stringsAsFactors = FALSE))
+}
